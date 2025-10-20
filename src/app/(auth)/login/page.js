17f138a303cdaada34c9c3a3394d0ced0f45be4f
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-10-20 22:25:03 +0545

    update

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -25,9 +25,13 @@ const LoginPage = () => {
 
         <form>
             <label className='block'>Email</label>
-             <input type="email" {...register("email")} placeholder="Enter a email" />
+             <input type="email" {...register("email"),{
+              required:"email is required"
+             }} placeholder="Enter a email" />
             <label className='block'>Password</label>
-             <input type="password" {...register("password")} placeholder="Enter a password" />
+             <input type="password" {...register("password"),{
+              required:"password is required"
+             }} placeholder="Enter a password" />
         <br></br>
         <button type="button" className="bg-amber-400 text-2xl text-amber-50 px-3.5 my-1" onClick={loginUser}>Login</button>
         </form>
