b3ad6cfef1eacb0c94adad40adbf1691fcb1fc2b
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-10-21 20:26:14 +0545

    handle submit using react hook

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -3,37 +3,34 @@ import { login } from "@/api/auth"
 import { useForm } from "react-hook-form";
 
 const LoginPage = () => {
-  const { register }=useForm();
+  const { register , handleSubmit}=useForm();
   const email=register("email");
 
 
-  async function loginUser(){
-    try {
-      const response=await login({
-        email:"hari@gmail.com",
-        password:"Test@12345",
+  async function submitForm(data){
+    console.log(data)
+    // try {
+    //   const response=await login({
+    //     email:"hari@gmail.com",
+    //     password:"Test@12345",
 
-      }) ;
-      console.log(response);
-    } catch (error) {
-      console.log(error)
-    }
+    //   }) ;
+    //   console.log(response);
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
 
   return (
     <div className='flex justify-center'>
 
-        <form>
+        <form onSubmit={handleSubmit(submitForm)}>
             <label className='block'>Email</label>
-             <input type="email" {...register("email"),{
-              required:"email is required"
-             }} placeholder="Enter a email" />
+             <input type="email" {...register("email")} placeholder="Enter a email" />
             <label className='block'>Password</label>
-             <input type="password" {...register("password"),{
-              required:"password is required"
-             }} placeholder="Enter a password" />
+             <input type="password" {...register("password")} placeholder="Enter a password" />
         <br></br>
-        <button type="button" className="bg-amber-400 text-2xl text-amber-50 px-3.5 my-1" onClick={loginUser}>Login</button>
+        <button type="button" className="bg-amber-400 text-2xl text-amber-50 px-3.5 my-1" onClick={submitForm}>Login</button>
         </form>
     </div>
   )
