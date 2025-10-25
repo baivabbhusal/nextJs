164ad96dfd3b7e76638d300da42226ec08d3ab91
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-10-25 23:50:26 +0545

    update

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -2,6 +2,7 @@
 import { login } from "@/api/auth";
 import { useForm } from "react-hook-form";
 import { EMAIL_REGEX } from "@/constants/regex";
+import Link from "next/link";
 
 const LoginPage = () => {
   const {
@@ -19,48 +20,13 @@ const LoginPage = () => {
       console.log(error);
     }
   }
-//   return (
-// <form className="max-w-sm mx-auto" onSubmit={handleSubmit(submitForm)}>
-//   <div className="mb-5">
-//     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-//     <input type="email" id="email"
-//     {...register("email", {
-//             required: "Email is required",
-//             pattern: {
-//               value: EMAIL_REGEX,
-//               message: "Invalid email address.",
-//             },
-//           })}
-//            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-//            <p>{errors.email?.message}</p>
-//   </div>
-//   <div className="mb-5">
-//     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-//     <input type="password" id="password"
-    //  {...register("password", {
-    //         required: "Password is required",
-    //         minLength:{
-    //           value:6,
-    //           message:"Password length must be greather than 6."
-    //         }
-    //       })}
-//           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-//           <p>{errors.password?.message}</p>
-//   </div>
-//   <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-// </form>
-
-
-
-
-//   );
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
       <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-      Flowbite    
+      Glaciers    
     </a>
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -71,12 +37,12 @@ const LoginPage = () => {
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
             <input type="email" 
-            {...register("email", {
-            required: "Invalid email address.",
-            minLength:{
-              value:6,
-              message:"Password length must be greather than 6."
-            }
+    {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: EMAIL_REGEX,
+              message: "Invalid email address.",
+            },
           })}
            id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
            <p>{errors.email?.message}</p>
@@ -107,7 +73,7 @@ const LoginPage = () => {
           </div>
           <button type="submit" className="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
           <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-            Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+            Don’t have an account yet? <Link href="r" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
           </p>
         </form>
       </div>
