86a564e367f9693d869dd9726c85ec55f3500bd2
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-10-29 22:39:14 +0545

    form validation

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,6 +1,8 @@
+"use client";
 import React from 'react'
-
+import { useForm } from "react-hook-form";
 const RegisterPage = () => {
+const { register } = useForm();
   return (
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -9,7 +11,15 @@ const RegisterPage = () => {
         <form className="space-y-4 md:space-y-6" action="#">
           <div>
             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+            <input type="email"   
+              {...register("email", {
+                        required: "Email is required",
+                        pattern: {
+                          value: EMAIL_REGEX,
+                          message: "Invalid email address.",
+                        },
+                        })}
+                         id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
           </div>
           <div>
             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
